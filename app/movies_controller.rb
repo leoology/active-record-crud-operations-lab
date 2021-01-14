7f96ebc3a945bba612d5e04373b1db9ce914af8f
Leo <efrenn98@gmail.com>
2021-01-13 18:29:00 -0800

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -6,7 +6,7 @@
 # end                              # end
 
 def can_be_instantiated_and_then_saved
-  movie = movie.new
+  movie = Movie.new
   movie.title = "This is a title."
   movie.save
 end
@@ -20,25 +20,26 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = moive.create(attributes)
+  movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block(title= "Home Alone", release_date=1990)
+def can_be_created_in_a_block(title: "Home Alone", release_date:1990)
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
   
   Movie.create do |m|
-    m.title="Home Alone"
-  end
+    m.title=title
+    m.release_date= release_date
+  end 
 end
 
 def can_get_the_first_item_in_the_database
-  Movie.first.title
+  Movie.first
 end
 
 def can_get_the_last_item_in_the_database
-  Movie.last.title
+  Movie.last
 end
 
 def can_get_size_of_the_database
@@ -46,14 +47,14 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  Movie.find(1).title
+  Movie.find(1)
 end
 
 def can_find_by_multiple_attributes
   Movie.find_by(title: "Title")
   Movie.find_by(release_date: "2000")
   Movie.find_by(director: "Me")
-  __
+
 end
 
 def can_find_using_where_clause_and_be_sorted
